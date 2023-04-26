1fc2ed8f19de2e5cd93af73f57a3878715390165
D3cipherd <108687433+D3cipherd@users.noreply.github.com>
2023-04-26 21:28:12 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,13 +3,15 @@
 rem Compiling java program
 javac ARXMLSort.java
 
-rem Run the program with 3 distinct test cases
+rem Not Valid Autosar File Case
 echo Case 1: .xml extension (test.xml)
 java ARXMLSort test.xml
 echo --------------------------------------------------------------
+rem Empty File Case
 echo Case 2: empty file (test_no_content.arxml)
 java ARXMLSort test_no_content.arxml
 echo --------------------------------------------------------------
+rem Normal Case
 echo Case 3: valid file (test_content.arxml)
 java ARXMLSort test_content.arxml
 echo --------------------------------------------------------------
